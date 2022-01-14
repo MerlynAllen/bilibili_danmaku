214daab1ace34f10e36747af5d9e6a50f056297b
Merlyn <xiaorui2001@outlook.com>
2022-01-14 15:46:15 +0800

    完成Danmaku类封装

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from http import client
+#! /usr/bin/python3
 import danmaku
 import asyncio
 import aiohttp
@@ -8,18 +8,19 @@ import logging as log
 LOGLEVEL = log.INFO
 log.basicConfig(
     level=LOGLEVEL, format="[%(asctime)s (%(levelname)s) line %(lineno)d in %(funcName)s]\n%(message)s\n----")
-
+client = None
 async def loop():
     global client
     while True:
         msg = client.get_message()
         if msg != None:
             print("[{username}]: {content}".format(**msg))
-        await asyncio.sleep(1)
+        await asyncio.sleep(0)
 async def main():
-    roomid = input("Room ID: ")
-    client = danmaku.Danmaku(roomid)
     lp = asyncio.create_task(loop())
     cn = asyncio.create_task(client.connect())
     await asyncio.gather(cn, lp)
-asyncio.run(main())
+if __name__ == "__main__":
+    roomid = input("Room ID: ")
+    client = danmaku.Danmaku(roomid)
+    asyncio.run(main())
